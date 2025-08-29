4eabe3895f3f06e85b4eaef5a1c5f75361c125b9
Henrique Galvão <henriquegalvao@discente.ufg.br>
2025-08-29 00:32:39 -0300

    Unigram [02].py
    The main idea of the project is to build a n-gram model. However, to start making progress, I built an unigram model for studying how its structure works an how should I apply changes to make it n-grams.

--- main.py
+++ main.py
@@ -1,21 +1,58 @@
+from binascii import Error
 import numpy as np
+import re
+from collections import Counter
+import random
+import PyPDF2
+from time import sleep
 
-class ngrams():
-    def __init__(self,N,corpus,data=[]):
-        self.N=N
-        self.corpus=corpus
-        self.data=data
-        
-    def defineData(self):
-        self.corpus.lower()
-        self.corpus.replace('!','').replace('.','').replace('?','').replace('"','').replace(',','').replace(';','').replace('(','').replace(')','')
-        self.data=self.corpus.split(' ')
-        self.data=[item.strip for item in self.data]
-        self.data=np.array(self.data)
+class unigram():
+
+  def __init__(self,corpus,data=[]):
+    self.corpus=corpus
+    self.data=data
+
+  def treatData(self):
+    self.corpus = self.corpus.lower()
+    self.corpus=re.sub(r"[^a-zA-ZÀ-ÿ\s]", "", self.corpus)
+    self.data=self.corpus.split(' ')
+    self.data = [item.strip() for item in self.data if item.strip() != ""]
+    self.data=np.array(self.data)
+  
+  def findWord(self,targetWord):
+    wordsAfter=[]
+    indices = np.array(np.where(self.data == targetWord)[0])
+
+    for i in indices:
+      if i + 1 < len(self.data):
+        wordsAfter.append(self.data[i+1])
     
-    def findWords(self,word):
-        wordsAfter=[]
-        indices = np.array(np.where(self.data == word)[0])
-        for i in indices:
-            wordsAfter.append(self.data[i+1])
-        return wordsAfter
+    highest_freq = max(Counter(wordsAfter).values())
+    most_frequent = [item for item, freq in Counter(wordsAfter).items() if freq == highest_freq]
+
+    return random.choice(most_frequent)
+
+def defineData(path):
+  text = ""
+  with open(path, "rb") as pdffile:
+    reader = PyPDF2.PdfReader(pdffile)
+    for page in reader.pages:
+      text += page.extract_text() + " "
+  return text
+
+model = unigram(defineData("ua00106a.pdf"),[])
+model.treatData()
+
+firstWord=input('Input the first word: ')
+
+try:
+  target=model.findWord(firstWord)
+  sentence=firstWord
+except:
+  raise Error("This word can't be used.")
+
+while True:
+  sentence+=" "+target
+  target=model.findWord(target)
+  print(sentence)
+  sleep(1)
