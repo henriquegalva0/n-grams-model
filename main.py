a5716a9a9cea7258a97cb89263d50826ccbbe8bb
Henrique Galvão <henriquegalvao@discente.ufg.br>
2025-08-30 12:12:43 -0300

    N-gram [07].py
    Finally the n-gram class is finished.
    The updates were:
    
    1. N-gram Class
     Now the kind of n-gram can be changed by giving the variable N to the class.
    2. Saving Data
     The instance "model" was created so as I can manage the strings better and access the upcoming words from the N variable.
    3. More Inputs
     The user can actively customize the n-gram model's max sentence length, N and first words to complete.
    
    The project would be 100% completed if it had punctuation and upper letters, although since the main objective was accomplished, I won't do more updates.

--- main.py
+++ main.py
@@ -1,83 +1,75 @@
 from binascii import Error
 import numpy as np
 import re
-from collections import Counter
+from collections import defaultdict, Counter
 import random
 import PyPDF2
 from time import sleep
 
-class unigram():
+class Ngram():
 
-  def __init__(self,corpus,maxlength,data=[]):
+  def __init__(self,corpus,maxlength,N=2,data=[]):
     self.corpus=corpus
     self.maxlength=maxlength
+    self.N=N
     self.data=data
+    self.model=defaultdict(list)
 
   def treatData(self):
-    self.corpus = self.corpus.lower()
-    self.corpus=re.sub(r"[^a-zA-ZÀ-ÿ\s]", "", self.corpus)
-    self.data=self.corpus.split(' ')
-    self.data = [item.strip() for item in self.data if item.strip() != ""]
+    self.corpus=self.corpus.lower()
+    self.corpus=re.sub(r"[^a-zA-ZÀ-ÿ\s]","",self.corpus)
+    self.data=[item.strip() for item in self.corpus.split(' ') if item.strip() != ""]
     self.data=np.array(self.data)
-  
-  def findWord(self,targetWord):
-    wordsAfter=[]
-    indices = np.array(np.where(self.data == targetWord)[0])
 
-    for i in indices:
-      if i + 1 < len(self.data):
-        wordsAfter.append(self.data[i+1])
-    
-    highest_freq = max(Counter(wordsAfter).values())
-    most_frequent = [item for item, freq in Counter(wordsAfter).items() if freq == highest_freq]
+    for i in range(len(self.data) - self.N+1):
+      key=tuple(self.data[i:i+self.N-1])
+      next_word=self.data[i+self.N-1]
+      self.model[key].append(next_word)
 
-    return random.choice(most_frequent)
-  
-  def findWord(self, targetWord):
-    wordsAfter = []
-    indices = np.where(self.data == targetWord)[0]
-
-    for i in indices:
-      if i + 1 < len(self.data):
-        wordsAfter.append(self.data[i+1])
-
-    if not wordsAfter:
+  def findWord(self, context):
+    if context not in self.model:
       return random.choice(self.data)
 
-    counter = Counter(wordsAfter)
-    return random.choices(list(counter.keys()), weights=counter.values())[0]
-  
+    counter=Counter(self.model[context])
+    return random.choices(list(counter.keys()),weights=counter.values())[0]
+
   def runModel(self):
-    self.maxlength=int(input('Input the max length: '))
-    firstWord=str(input('Input the first word: '))
-    target=self.findWord(firstWord)
-    sentence=firstWord
+    self.maxlength = int(input('Input the max length: '))
+    firstWords = input(f'Input the first {self.N-1} word(s): ').split()
+
+    if len(firstWords)<self.N-1:
+      raise Error(f"You must input atleast {self.N-1} initial word(s).")
+
+    context=tuple(firstWords[-(self.N-1):])
+    sentence=list(firstWords)
 
     while len(sentence)<self.maxlength:
-      sentence+=" "+target
-      target=model.findWord(target)
-      print(sentence,end="\n")
-      sleep(0.2)
-    return str(sentence)
+      next_word=self.findWord(context)
+      sentence.append(next_word)
+      context=tuple(sentence[-(self.N-1):])
+      print(" ".join(sentence), end="\n")
+      sleep(0.1)
 
-  def getFullText(self,results):
+    return " ".join(sentence)
+
+  def getFullText(self, results):
     fullText=''
     for letter in results:
-      if letter != "\n":
+      if letter!="\n":
         fullText+=letter
       else:
         fullText+=" "
     return fullText
 
 def defineData(path):
-  text = ""
+  text=""
   with open(path, "rb") as pdffile:
     reader = PyPDF2.PdfReader(pdffile)
     for page in reader.pages:
       text += page.extract_text() + " "
   return text
 
-model = unigram(defineData("ua00106a.pdf"),200,[])
+model=Ngram(defineData("ua00106a.pdf"), None, N=int(input("Input the target N of the N-grams model. "))+1)
 model.treatData()
 
-print("\n\n-=- Full text -=-\n\n",model.getFullText(model.runModel()))
+print("\n\n-=- Full text -=-\n\n", model.getFullText(model.runModel()))
